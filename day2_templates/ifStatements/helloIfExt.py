2d1d55670bbb56ddc56a20dde3c5f2c07718af1a
Sofia Lindholm <sofia.lindholm@myy.haaga-helia.fi>
2021-05-25 12:12:18 +0300

    teht3 ifTemplate

---
+++ day2_templates/ifStatements/helloIfExt.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('base.html')
+
+
+@app.route('/foo')
+def foo():
+	return render_template('foo.html')
+
+@app.route('/pants')
+def pants():
+	pants = [ "green pants" , "jeans" ]
+	return render_template('pants.html', pants=pants)
+
+if __name__ == '__main__':
+	app.run()

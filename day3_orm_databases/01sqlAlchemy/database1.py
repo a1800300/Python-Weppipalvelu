3ceed73ed4571ad6b2bae4d43e4cdfebd17d3771
Sofia Lindholm <sofia.lindholm@myy.haaga-helia.fi>
2021-05-26 10:51:43 +0300

    lisätty dataa ja looppi

--- day3_orm_databases/01sqlAlchemy/database1.py
+++ day3_orm_databases/01sqlAlchemy/database1.py
@@ -6,16 +6,21 @@ db = SQLAlchemy(app)
 
 class Countries(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	country  = db.Column(db.String, nullable=False)
+	countryName  = db.Column(db.String, nullable=False)
 	population = db.Column(db.Integer, nullable=False)	
 
 @app.before_first_request
 def initMe():
 	db.create_all()
+	# Let's add some test data here
+	country =  Countries(countryName='Finland', population='5180000')
+	db.session.add(country)
+	db.session.commit()
 
 @app.route('/')
 def index():
-	return render_template('base.html')
+	countries =  Countries.query.all()
+	return render_template('loopData.html', countries=countries)
 
 if __name__ == '__main__':
 	app.run()
